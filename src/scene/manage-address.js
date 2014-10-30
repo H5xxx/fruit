c8ed82e3010da62631389343ce6622b5240f0a9c
Hanxing Yang <nighca@live.cn>
2014-10-30 22:11:58 +0800

    manage address page

--- src/scene/manage-address.js
+++ src/scene/manage-address.js
@@ -3,6 +3,9 @@
  */
 
 define(function(require, exports) {
+	var util = require('../util');
+    var AddressModel = require('../model/address');
+    var Popup = require('../widget/popup');
 
     var ManageAddress = require('../proto/scene').sub({
 
@@ -10,7 +13,63 @@ define(function(require, exports) {
 
         title: '地址管理',
 
-        template: 'manage-address'
+        template: 'manage-address',
+
+        getData: function(params, callback) {
+
+            util.finish([
+
+                AddressModel.fetch.bind(AddressModel, params)
+
+            ], function(addressList){
+
+                callback(null, {
+                    addressList: addressList
+                });
+
+            });
+
+        },
+
+        render: function(params){
+            ManageAddress.__super__.render.apply(this, arguments);
+
+            this.initAddressList();
+
+            this.initBar();
+        },
+
+        initAddressList: function(){
+            var me = this,
+                addressItems = this.el.find('.j-address-item');
+        },
+
+        initBar: function(){
+            var me = this;
+                barDom = this.el.find('.j-bar'),
+                next = barDom.find('.j-next');
+
+            next.on('tap', function(e){
+                me.createAddress(function(err, address){
+                    if(err){
+                        Popup.alert(err);
+                    }else{
+                    	// TODO: refresh address list
+                    }
+                });
+            });
+        },
+
+        createAddress: function(callback){
+            AddressModel.createRemotely({
+                city: this.el.find('.j-city').val(),
+                country: this.el.find('.j-country').val(),
+                detail: this.el.find('.j-detail').val(),
+                consignee: this.el.find('.j-consignee').val(),
+                phone: this.el.find('.j-phone').val(),
+                isDefault: true
+            }, callback);
+        }
     });
 
     return ManageAddress;
--- src/tpl/dist/.cache/manage-address.js
+++ src/tpl/dist/.cache/manage-address.js
@@ -1,2 +1,27 @@
-/*TMODJS:{"version":6,"md5":"d34de732b32149d238d642efcc756b66"}*/
-template('manage-address','<ul class="container-cnt items"> <li class="address-item"> <div class="item-cnt" to="address-choice1"> <h5 class="item-title">[默认] 张先生，17283192011</h5> <p class="item-desc"> 上海市杨浦区邯郸路113号 <span class="set-default">设为默认</span> </p> </div> </li> <li class="address-item"> <div class="item-cnt" to="address-choice2"> <h5 class="item-title">王先生，11111111111</h5> <p class="item-desc"> 北京市东城区某某路120号1号楼102 <span class="set-default">设为默认</span> </p> </div> </li> <li class="address-item new-item"> <div class="item-cnt" to="address-choice2"> <h5 class="item-title">使用新的地址</h5> </div> <div class="input-block"> <div class="input-line"> <select class="short"> <option>北京市</option> </select> <select class="short"> <option>东城区</option> </select> <input class="long" type="text" name="address" value="xx路30号"> </div> <div class="input-line"> <input class="long" type="text" name="name" value="汪先生"> </div> <div class="input-line"> <input class="long" type="text" name="phone" value="18212334123"> </div> </div> </li> </ul> <div class="bar"> <p class="cnt">添加该地址？</p> <i class="j-nav op" data-nav="/personal"></i> </div>');
+/*TMODJS:{"version":7,"md5":"22425313fdddea82ea01d204873cbe8d"}*/
+template('manage-address',function($data,$filename) {
+'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,addressList=$data.addressList,address=$data.address,i=$data.i,$escape=$utils.$escape,$out='';$out+='<ul class="container-cnt items"> ';
+$each(addressList,function(address,i){
+$out+=' <li class="j-address-item';
+if(address.isDefault){
+$out+=' j-default';
+}
+$out+=' address-item" data-address-id="';
+$out+=$escape(address.id);
+$out+='"> <div class="item-cnt"> <h5 class="item-title">';
+if(address.isDefault){
+$out+='[默认] ';
+}
+$out+=$escape(address.consignee);
+$out+='，';
+$out+=$escape(address.phone);
+$out+='</h5> <p class="item-desc"> ';
+$out+=$escape(address.province);
+$out+=$escape(address.city);
+$out+=$escape(address.country);
+$out+=$escape(address.detail);
+$out+=' <span class="set-default">设为默认</span> </p> </div> </li> ';
+});
+$out+=' <li class="address-item new-item"> <div class="item-cnt" to="address-choice2"> <h5 class="item-title">使用新的地址</h5> </div> <div class="input-block"> <div class="input-line"> <select class="j-city short"> <option>市</option> </select> <select class="j-country short"> <option>区</option> </select> <input class="j-detail long" type="text" value="xx路30号"> </div> <div class="input-line"> <input class="j-consignee long" type="text" value="汪先生"> </div> <div class="input-line"> <input class="j-phone long" type="number" value="18212334123"> </div> </div> </li> </ul> <div class="bar"> <p class="cnt">添加该地址？</p> <i class="j-next op"></i> </div>';
+return new String($out);
+});
