bd29975ae966da9a87b30fc9f309930f52b4a8f7
Hanxing Yang <nighca@live.cn>
2014-11-01 21:49:24 +0800

    add link to phone

--- src/tpl/dist/.cache/home.js
+++ src/tpl/dist/.cache/home.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":12,"md5":"7df8238ac79edde439f7661df59d044c"}*/
+/*TMODJS:{"version":13,"md5":"748518c63b20736770cfd6e4d1561077"}*/
 template('home',function($data,$filename) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$escape=$utils.$escape,notice=$data.notice,$out='';$out+='<div class="container-cnt"> <div class="logo-block"></div> <h3 class="notice"> <i class="icon icon-notice"></i>';
 $out+=$escape(notice.title);
@@ -10,9 +10,11 @@ $out+=' </p> </li> <li class="info-line"> <h5 class="info-title">联系我们</h
 $out+=$escape(notice.address);
 $out+='</li> <li><i class="icon icon-mail"></i>';
 $out+=$escape(notice.email);
-$out+='</li> <li><i class="icon icon-phone"></i>';
+$out+='</li> <li><i class="icon icon-phone"></i><a rel="nofollow" href="http://tel:';
 $out+=$escape(notice.telphone);
-$out+='</li> </ul> </li> </ul> <ul class="info-block"> <li class="info-line"> <h5 class="info-title">购买须知</h5> <p class="info-cnt"> ';
+$out+='" target="_blank">';
+$out+=$escape(notice.telphone);
+$out+='</a></li> </ul> </li> </ul> <ul class="info-block"> <li class="info-line"> <h5 class="info-title">购买须知</h5> <p class="info-cnt"> ';
 $out+=$escape(notice.notes);
 $out+=' </p> </li> </ul> <ul class="info-block"> <li class="info-line"> <h5 class="info-title">配送信息</h5> <p class="info-cnt"> ';
 $out+=$escape(notice.distribution);
