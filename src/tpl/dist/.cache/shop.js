8784cc15e570f1d810e834c32799f9a8e3d783c6
Hanxing Yang <nighca@live.cn>
2014-11-04 22:19:01 +0800

    add scroll fro shop side & top

--- src/tpl/dist/.cache/shop.js
+++ src/tpl/dist/.cache/shop.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":42,"md5":"90ae19151ab3c0e91e9f798a4292c565"}*/
+/*TMODJS:{"version":43,"md5":"564b7c9b82b10800e2525f5f5722a309"}*/
 template('shop',function($data,$filename) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,categories=$data.categories,category=$data.category,i=$data.i,$escape=$utils.$escape,$out='';$out+='<div class="side-cnt"> <div class="up-line logo"></div> <ul class="side-items"> ';
 $each(categories,function(category,i){
@@ -12,6 +12,6 @@ $out+='"> <span class="item-cnt">';
 $out+=$escape(category.name);
 $out+='</span> </li> ';
 });
-$out+=' </ul> </div> <div class="main-cnt"> <div class="j-package-list-wrapper"> </div> <div class="j-fruit-list-wrapper"> </div> </div> <div class="j-bar bar"> <p class="cnt">已选购 <span class="j-num num">3</span> 件，共 <span class="j-sum sum">15</span> 元</p> <i class="j-next op icon icon-tick"></i> </div>';
+$out+=' </ul> </div> <div class="main-cnt"> <div class="j-package-list-wrapper sub-opts-wrapper"> </div> <div class="j-fruit-list-wrapper"> </div> </div> <div class="j-bar bar"> <p class="cnt">已选购 <span class="j-num num">3</span> 件，共 <span class="j-sum sum">15</span> 元</p> <i class="j-next op icon icon-tick"></i> </div>';
 return new String($out);
 });
